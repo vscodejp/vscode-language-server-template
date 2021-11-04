21db360668c5fe958073f4e9f434c7292d2a553a
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-11-04 17:12:27 +0900

    add linter implementation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 'use strict';
 
 import {
+	CodeAction,
+	CodeActionKind,
 	createConnection,
 	Diagnostic,
 	DiagnosticSeverity,
 	ProposedFeatures,
 	Range,
 	TextDocuments,
+	TextDocumentEdit,
 	TextDocumentSyncKind,
+	TextEdit,
+	VersionedTextDocumentIdentifier
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
+
+// コマンド識別子
+namespace CommandIDs {
+	export const fix = 'sample.fix';
+}
 
 // サーバー接続オブジェクトを作成する。この接続にはNodeのIPC(プロセス間通信)を利用する
 // LSPの全機能を提供する
@@ -39,7 +49,14 @@ connection.onInitialize((_params, _cancel, progress) => {
 				save: {
 					includeText: false,
 				}
-			}
+			},
+			codeActionProvider: {
+				codeActionKinds: [CodeActionKind.QuickFix],
+				resolveProvider: true
+			},
+			executeCommandProvider: {
+				commands: [CommandIDs.fix],
+			},
 		},
 	};
 });
@@ -51,23 +68,36 @@ connection.onInitialize((_params, _cancel, progress) => {
 function validate(doc: TextDocument) {
 	// 警告などの状態を管理するリスト
 	const diagnostics: Diagnostic[] = [];
-	// 0行目(エディタ上の行番号は1から)の端から端までに警告
-	const range: Range = {start: {line: 0, character: 0},
-		end: {line: 0, character: Number.MAX_VALUE}};
-	// 警告を追加する
-	const diagnostic: Diagnostic = {
-		// 警告範囲
-		range: range,
-		// 警告メッセージ
-		message: 'Hello world',
-		// 警告の重要度、Error, Warning, Information, Hintのいずれかを選ぶ
-		severity: DiagnosticSeverity.Warning,
-		// 警告コード、警告コードを識別するために使用する
-		code: '',
-		// 警告を発行したソース、例: eslint, typescript
-		source: 'sample',
-	};
-	diagnostics.push(diagnostic);
+
+	// ２つ以上並んでいるアルファベット大文字を検出
+	const text = doc.getText();
+	// 検出するための正規表現 (正規表現テスト: https://regex101.com/r/wXZbr9/1)
+	const pattern = /\b[A-Z]{2,}\b/g;
+	let m: RegExpExecArray | null;
+
+	// 正規表現にマッチした文字列すべてを対象にする
+	while ((m = pattern.exec(text)) !== null) {
+		// 対象の位置から正規表現にマッチした文字までを対象にする
+		const range: Range = {
+			start: doc.positionAt(m.index),
+			end: doc.positionAt(m.index + m[0].length),
+		};
+		// 警告を追加する;
+		const diagnostic: Diagnostic = {
+			// 警告範囲
+			range: range,
+			// 警告メッセージ
+			message: `${m[0]} is all UPPERCASE.`,
+			// 警告の重要度、Error, Warning, Information, Hintのいずれかを選ぶ
+			severity: DiagnosticSeverity.Warning,
+			// 警告コード、警告コードを識別するために使用する
+			code: '',
+			// 警告を発行したソース、例: eslint, typescript
+			source: 'sample',
+		};
+		// 警告リストに警告内容を追加
+		diagnostics.push(diagnostic);
+	}
 	//接続に警告を通知する
 	connection.sendDiagnostics({ uri: doc.uri, diagnostics });
 }
@@ -101,6 +131,51 @@ function setupDocumentsListeners() {
 		const uri = close.document.uri;
 		// 警告を削除する
 		connection.sendDiagnostics({ uri: uri, diagnostics: []});
+	});
+
+	// Code Actionを追加する
+	connection.onCodeAction((params) => {
+		// sampleから生成した警告のみを対象とする
+		const diagnostics = params.context.diagnostics.filter((diag) => diag.source === 'sample');
+
+		connection.console.info(`Code Actions is added`);
+		// 対象ファイルを取得する
+		const textDocument = documents.get(params.textDocument.uri);
+
+		// 対象ファイルが存在しない場合は終了する
+		if (textDocument === undefined || diagnostics.length === 0) {
+			return [];
+		}
+		const codeActions: CodeAction[] = [];
+		// 各警告に対してアクションを生成する
+		diagnostics.forEach((diag) => {
+			// アクションのメッセージ
+			const title = 'Fix to lower case';
+			// 警告範囲の文字列取得
+			const originalText = textDocument.getText(diag.range);
+			// 該当箇所を小文字に変更
+			const edits = [TextEdit.replace(diag.range, originalText.toLowerCase())];
+
+			const textDocumentIdentifier: VersionedTextDocumentIdentifier = {
+				uri: textDocument.uri,
+				version: textDocument.version};
+			const editPattern = {
+				documentChanges: [
+					TextDocumentEdit.create(textDocumentIdentifier, edits)
+				]
+			};
+			// コードアクションを生成
+			const fixAction = CodeAction.create(
+				title,
+				editPattern,
+				CodeActionKind.QuickFix);
+			// コードアクションと警告を関連付ける
+			fixAction.diagnostics = [diag];
+			fixAction.isPreferred = true;
+			codeActions.push(fixAction);
+		});
+
+		return codeActions;
 	});
 }
 

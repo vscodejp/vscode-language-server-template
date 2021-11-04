8136d73e695481b597daaea7e566d2d4db36afee
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-11-04 14:21:47 +0900

    make skelton comments

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,8 +10,12 @@ import {
 
 // 拡張機能が有効になったときに呼ばれる
 export function activate(context: ExtensionContext): void {
+	// サーバーのパスを取得
 	const serverModule =  Uri.joinPath(context.extensionUri, 'server', 'out', 'server.js').fsPath;
+	// デバッグ時の設定
 	const debugOptions = { execArgv: ['--nolazy', '--inspect=6011'], cwd: process.cwd() };
+
+	// サーバーの設定
 	const serverOptions: ServerOptions = {
 		run: {
 			module: serverModule,
@@ -24,6 +28,7 @@ export function activate(context: ExtensionContext): void {
 			options: debugOptions,
 		},
 	};
+	// LSPとの通信に使うリクエストを定義
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [
 			{ scheme: 'file' },
@@ -37,12 +42,14 @@ export function activate(context: ExtensionContext): void {
 
 	let client: LanguageClient;
 	try {
+		// LSPを起動
 		client = new LanguageClient('Sample LSP Server', serverOptions, clientOptions);
 	} catch (err) {
 		void Window.showErrorMessage('拡張機能の起動に失敗しました。詳細はアウトプットパネルを参照ください');
 		return;
 	}
 
+	// 拡張機能のコマンドを登録
 	context.subscriptions.push(
 		client.start(),
 	);
--- server/src/server.ts
+++ server/src/server.ts
@@ -11,20 +11,26 @@ import {
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
-// サーバーのコネクションを作成する。このコネクションはトランスポートはNodeのIPC(プロセス間通信)を利用する
+// サーバー接続オブジェクトを作成する。この接続にはNodeのIPC(プロセス間通信)を利用する
 // LSPの全機能を提供する
 const connection = createConnection(ProposedFeatures.all);
 connection.console.info(`Sample server running in node ${process.version}`);
+// 初期化ハンドルでインスタンス化する
 let documents!: TextDocuments<TextDocument>;
 
 connection.onInitialize((_params, _cancel, progress) => {
+	// サーバーの起動を進捗表示する
 	progress.begin('Initializing Sample Server');
+	// テキストドキュメントを監視する
 	documents = new TextDocuments(TextDocument);
 	setupDocumentsListeners();
+	// 起動進捗表示の終了
 	progress.done();
 
 	return {
+		// サーバー仕様
 		capabilities: {
+			// ドキュメントの同期
 			textDocumentSync: {
 				openClose: true,
 				change: TextDocumentSyncKind.Incremental,
